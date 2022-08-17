b4ad6498aae1c9614d4d39be1210ff6ff4bfaa8a
josedleonett <josed@gmail.com>
2022-08-16 23:18:08 -0300

    ENCRIPTADOR - javascript conectado

---
+++ Principiante en Programación G3 - ONE/7 - Encara los Challenges de Principiante en Programación/Encriptador/scripts/script.js
@@ -0,0 +1,13 @@
+function encriptar(accion) {
+  event.preventDefault();
+
+  document.getElementById("articuloSinResultado").style.display = "none";
+
+  document.getElementById("articuloResultado").style.display = "flex";
+
+  if (accion === "encriptar") {
+    //funcion ENCRIPTAR
+  } else {
+    //funcion DESENCRIPTAR
+  }
+}
